f17a4579e72bf85c3db4580a207295f35050a00d
SlickNick17 <38701213+SlickNick17@users.noreply.github.com>
2018-05-13 18:44:50 -0400

    Add files via upload

---
+++ Sources/Tanks/MessageCenter.swift
@@ -0,0 +1,30 @@
+//
+//  MessageCenter.swift
+//  TankLand
+//
+//  Created by Nicholas Hatzis-Schoch on 5/11/18.
+//  Copyright © 2018 Slick Games. All rights reserved.
+//
+
+import Foundation
+
+struct MessageCenter: CustomStringConvertible{
+    var messageContainer: [Int: String]
+    init(){
+        self.messageContainer = [Int: String]()
+    }
+    mutating func sendMessage(id: Int, message: String){
+        messageContainer[id] = message
+    }
+    func receiveMessage(id: Int)->String{
+        return messageContainer[id]!
+    }
+    var description: String{
+        var desc = ""
+        for e in messageContainer{
+            desc.append("ID: \(e.0) Message: \(e.1)")
+            desc.append("\n")
+        }
+        return desc
+    }
+}
