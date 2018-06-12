165d05c447b4cdf970929178b440f9b3808f56e4
Ultimat0 <marknazzaro2@gmail.com>
2018-06-12 12:11:44 -0400

    done now?

--- Sources/Tanks/Tank.swift
+++ Sources/Tanks/Tank.swift
@@ -2,9 +2,7 @@ import Foundation
 
 class Tank: GameObject {
 
-	enum Mode {
-		case GUNRUN, CHILL
-	}
+	
 
 	private (set) var shields: Int = 0
 	var radarResults: [RadarResult]?
@@ -12,11 +10,7 @@ class Tank: GameObject {
 	private (set) var preActions = [Actions : PreAction]()
 	private (set) var postActions = [Actions: PostAction]()
 	private let initialInstructions: String?
-	private var turn = 0
-	private var mode: Mode = .GUNRUN
-	private var chillin = false
-
-
+	
 	init (row: Int, col: Int, energy: Int, id: String, instructions: String) {
 		initialInstructions = instructions
 		super.init(row: row, col: col, objectType: .Tank, energy: energy, id: id)
@@ -30,38 +24,9 @@ class Tank: GameObject {
 	final func receiveMessage (message: String?) {receivedMessage = message}
 
 	func computePreActions() {
-		turn += 1
-		if let r = radarResults {
-			if turn >= 100 || chillin || r.count == 0{
-				mode = .CHILL
-				chillin = true
-			}
-		}
-		else {
-			mode = .GUNRUN
-		}
-
-		switch mode{
-			case .CHILL: break
-			case .GUNRUN: addPreAction(preAction: RadarAction(range: 4))
-			default: break
-		}
 	}
 
 	func computePostActions() {
-
-		switch mode {
-			case .CHILL: break
-			case .GUNRUN: 
-				for i in radarResults! {
-					if !isFriendly(tank: i){
-						addPostAction(postAction: MissileAction(power: (i.energy) / 10 + 30, target: i.position))
-						break
-					}
-				}
-				addPostAction(postAction: MoveAction(distance: 2, direction: randomDirection()))
-			default: break;
-		}
 	}
 
 	final func addPreAction (preAction: PreAction) {
@@ -82,22 +47,5 @@ class Tank: GameObject {
 		self.receivedMessage = receivedMessage
 	}
 
-	func randomDirection () -> Direction {
-		let dirNum = Int(random() % 8)
-		var dir: Direction = .N
-		if dirNum == 0 {dir = .N}
-		else if dirNum == 1 {dir = .NE}
-		else if dirNum == 2 {dir = .E}
-		else if dirNum == 3 {dir = .SE}
-		else if dirNum == 4 {dir = .S}
-		else if dirNum == 5 {dir = .SW}
-		else if dirNum == 6 {dir = .W}
-		else {dir = .NW}
-		return dir
-	}
-
-	func isFriendly(tank: RadarResult) -> Bool{
-		return tank.id == id
-	}
-
+	
 }
